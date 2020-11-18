a512d82025cd9ac5b165b7431792d82fcc0e9d94
Шевченко Максим <shevchenko@gogpb.ru>
2020-11-18 16:51:40 +0200

    new branch

--- app.js
+++ app.js
@@ -17,7 +17,6 @@ mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology
     logger.error('error connecting to MongoDB:', error.message)
   })
 
-
 app.use(cors())
 app.use(express.static('build'))
 app.use(express.json())
